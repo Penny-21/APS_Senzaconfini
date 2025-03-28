da5a9822f46bce6b8add56259366790041ea5e15
Penny-21 <vpennacchio02@gmail.com>
2025-03-28 02:00:37 +0100

    Migration from html to php plus footer and header components finished

---
+++ index.php
@@ -0,0 +1,170 @@
+<!doctype html>
+
+<html
+  lang="en"
+  class="layout-navbar-fixed layout-menu-fixed layout-compact"
+  dir="ltr"
+  data-skin="default"
+  data-assets-path="assets/"
+  data-template="horizontal-menu-template-no-customizer-starter"
+  data-bs-theme="light">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <title>APS SenzaConfini</title>
+
+    <meta name="description" content="" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon/favicon.ico" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap"
+      rel="stylesheet" />
+
+    <link rel="stylesheet" href="assets/vendor/fonts/iconify-icons.css" />
+
+    <!-- Core CSS -->
+    <!-- build:css assets/vendor/css/theme.css  -->
+
+    <link rel="stylesheet" href="assets/vendor/libs/pickr/pickr-themes.css" />
+    <link rel="stylesheet" href="assets/vendor/libs/node-waves/node-waves.css" />
+
+    <link rel="stylesheet" href="assets/vendor/css/core.css" />
+    <link rel="stylesheet" href="assets/css/demo.css" />
+
+    <!-- Vendors CSS -->
+
+    <link rel="stylesheet" href="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+
+    <!-- endbuild -->
+
+    <!-- Page CSS -->
+
+    <!-- Helpers -->
+    <script src="assets/vendor/js/helpers.js"></script>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+
+    <script src="assets/js/config.js"></script>
+  </head>
+
+  <body>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-navbar-full layout-horizontal layout-without-menu">
+      <div class="layout-container">
+        <!-- Navbar -->
+        <?php include 'header.php'; ?>
+        <!-- / Navbar -->
+
+        <!-- Layout container -->
+        <div class="layout-page">
+          <!-- Content wrapper -->
+          <div class="content-wrapper">
+            <!-- Menu -->
+            <aside id="layout-menu" class="layout-menu-horizontal menu-horizontal menu flex-grow-0">
+              <div class="container-xxl d-flex h-100">
+                <ul class="menu-inner py-1">
+                  <!-- Page -->
+                  <li class="menu-item active">
+                    <a href="index.html" class="menu-link">
+                      <i class="menu-icon icon-base ti tabler-smart-home"></i>
+                      <div data-i18n="Home">Home</div>
+                    </a>
+                  </li>
+                  <li class="menu-item">
+                    <a href="modules/about-us.html" class="menu-link">
+                      <i class="menu-icon icon-base ti tabler-info-octagon"></i>
+                      <div data-i18n="About Us">About Us</div>
+                    </a>
+                  </li>
+                  <li class="menu-item">
+                    <a href="modules/our-team.html" class="menu-link">
+                      <i class="menu-icon icon-base ti tabler-users-group"></i>
+                      <div data-i18n="Our Team">Our Team</div>
+                    </a>
+                  </li>
+                  <li class="menu-item">
+                    <a href="modules/our-projects.html" class="menu-link">
+                      <i class="menu-icon icon-base ti tabler-briefcase"></i>
+                      <div data-i18n="Our Projects">Our Projects</div>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </aside>
+            <!-- / Menu -->
+
+            <!-- Content -->
+            <div class="container-xxl flex-grow-1 container-p-y">
+              <h4 class="py-4 mb-6">Page 1</h4>
+              <p>
+                Sample page.<br />For more layout options use
+
+                <a
+                  href="https://demos.pixinvent.com/vuexy-html-admin-template/documentation//layouts.html"
+                  target="_blank"
+                  class="fw-medium"
+                  >Layout docs</a
+                >.
+              </p>
+            </div>
+            <!--/ Content -->
+
+            <!-- Footer -->
+            <?php include 'footer.php'; ?>
+            <!-- / Footer -->
+
+            <div class="content-backdrop fade"></div>
+          </div>
+          <!--/ Content wrapper -->
+        </div>
+
+        <!--/ Layout container -->
+      </div>
+    </div>
+
+    <!-- Overlay -->
+    <div class="layout-overlay layout-menu-toggle"></div>
+
+    <!-- Drag Target Area To SlideIn Menu On Small Screens -->
+    <div class="drag-target"></div>
+
+    <!--/ Layout wrapper -->
+
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/theme.js -->
+
+    <script src="assets/vendor/libs/jquery/jquery.js"></script>
+
+    <script src="assets/vendor/libs/popper/popper.js"></script>
+    <script src="assets/vendor/js/bootstrap.js"></script>
+    <script src="assets/vendor/libs/node-waves/node-waves.js"></script>
+
+    <script src="assets/vendor/libs/@algolia/autocomplete-js.js"></script>
+
+    <script src="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+    <script src="assets/vendor/libs/hammer/hammer.js"></script>
+
+    <script src="assets/vendor/libs/i18n/i18n.js"></script>
+    <script src="assets/vendor/js/menu.js"></script>
+
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+
+    <!-- Main JS -->
+
+    <script src="assets/js/main.js"></script>
+
+    <!-- Page JS -->
+  </body>
+</html>
