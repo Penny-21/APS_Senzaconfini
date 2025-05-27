c498689fbd4ff964f7a275147798a949bf336c23
Penny-21 <vpennacchio02@gmail.com>
2025-05-27 08:52:42 +0200

    Remove hero background images from multiple sections for cleaner design

--- modules/about-us.php
+++ modules/about-us.php
@@ -113,11 +113,6 @@
             <!-- Hero: Start -->
             <section id="hero-animation">
               <div id="landingHero" class="section-py landing-hero position-relative">
-                <img
-                  src="../assets/img/front-pages/backgrounds/hero-bg.png"
-                  alt="hero background"
-                  class="position-absolute top-0 start-50 translate-middle-x object-fit-cover w-100 h-100"
-                  data-speed="1" />
                 <div class="container">
                   <div class="hero-text-box text-center position-relative">
                     <h1 class="text-primary hero-title display-6 fw-extrabold">
